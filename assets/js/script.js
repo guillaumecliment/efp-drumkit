b3f31fe9e9be7054154f615ddffc90c6c7104e52
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2022-12-09 00:32:59 +0100

    progressing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,75 @@
 //TODO: create a container div (instruments)
 
 const container = document.createElement('div');
-container.setAttribute('class', container.getAttribute('container'))
-
-document.getElementById('drum-holder').appendChild(instrumentsContainer)
+container.setAttribute('class', 'container')
 
 const sounds =
 [
-        {
-            letter: 'A',
-            sound: 'Clap',
-            url: ''
-        },
+    {
+        letter: 'A',
+        sound: 'Clap',
+        url: ''
+    },
 
-        {
-            letter: "S",
-            sound: "Hi Hat",
-            url: ''
-        },
-            
-        {
-            letter: "D",
-            sound: "Kick",
-            url: ''
-        },
+    {
+        letter: "S",
+        sound: "Hi Hat",
+        url: ''
+    },
+        
+    {
+        letter: "D",
+        sound: "Kick",
+        url: ''
+    },
 
-        {
-            letter: "F",
-            sound: "Open Hat",
-            url: ''
-        },
+    {
+        letter: "F",
+        sound: "Open Hat",
+        url: ''
+    },
 
-        {
-            letter: "G",
-            sound: "Boom",
-            url: ''
-        },
+    {
+        letter: "G",
+        sound: "Boom",
+        url: ''
+    },
 
-        {
-            letter: "H",
-            sound: "Ride",
-            url: ''
-        },
+    {
+        letter: "H",
+        sound: "Ride",
+        url: ''
+    },
 
-        {
-            letter: "J",
-            sound: "Snare",
-            url: ''
-        },
+    {
+        letter: "J",
+        sound: "Snare",
+        url: ''
+    },
 
-        {
-            letter: "K",
-            sound: "Tom",
-            url: ''
-        },
+    {
+        letter: "K",
+        sound: "Tom",
+        url: ''
+    },
 
-        {
-            letter: "L",
-            sound: "Tink",
-            url: ''
-        }
+    {
+        letter: "L",
+        sound: "Tink",
+        url: ''
+    }
 ]
+
+document.getElementById('drum-holder').appendChild(container)
+
 
 for (const sound of sounds)
 {
-    console.log(letter, sound, url);
+    let soundName = sound.letter;
+
+    container.createElement('div');
+    container.div.setAttribute('class', soundName))
+    
 }
 
 console.log("test finish!")
@@ -72,9 +77,4 @@ console.log("test finish!")
 
 
 //TODO: use a loop to create 3 lines of 3 div (en utilisant du flexbox et des gap)
-    // class "instrument"
-    // an sound url
-    // adding to each of them the htm text (ex: "J<br>Snare")
-
-
-//TODO: creat an event listener onkeymachin
+//TODO: create an event listener onkey...machin
